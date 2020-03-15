f8cb313bf61313f7a1c3d93cf5762860ab026bcf
cjber <cjberragan@gmail.com>
2020-03-15 16:19:57 +0000

    add cjber home layout dz60 v1

--- keyboards/dztech/dz60rgb_ansi/keymaps/cjber/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/cjber/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT_60_ansi(
         KC_GESC  , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______  , _______, _______, _______, _______, _______, KC_CALC, _______, KC_INS,  _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
+        _______  , _______, _______, _______, _______, _______, _______, _______, _______,  _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
         KC_GRAVE , _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PGDN, KC_PGUP,          _______,
         _______  , KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, KC_END,  KC_HOME,          _______,
         _______  , _______, _______,                            _______,                            _______, _______, _______, _______
